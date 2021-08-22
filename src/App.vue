ec3d1819c5dd6a70797c7431f3e5d7edb896bcfe
shahedi.s <saharr.shahedi@gmail.com>
2021-08-22 23:56:32 +0430

    add skills component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Hiiiiii"/>
+  <Skills/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Skills from './components/Skills.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Skills
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
